61d3e726399ec500391087f6621d9d21df66465c
fishylunar <f1skdev@gmail.com>
2025-07-31 11:46:18 +0200

    fixing build order stuff

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# fuck you i cant be bothered to write anything cool
+npm install drizzle-kit turbo
+rm -rf shared/drizzle/
+cd packages/database
+npm run migrate
+cd ../../
+echo "You should be able to build and run the server now."
+echo "Try: npm run build <--- to build the server"
+echo "Then: npm run start <--- to start the server"
+echo "Please make sure to set the environment vars in each app's .env.production / .env.development file."
+echo "If confused, check the readme file :3"
